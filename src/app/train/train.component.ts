d1364cb58afc06f1b3a5f12646388f185033c144
SamGVanN <samuel.vannier@hotmail.fr>
2020-07-24 13:37:48 +0200

    RxJS
    Nous permet de créer nos propres observables,
    met à disposition plein d'opérateurs

--- src/app/train/train.component.ts
+++ src/app/train/train.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { timer } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 
 @Component({
@@ -20,6 +22,18 @@ export class TrainComponent implements OnInit {
         (value) => this.trainData = value // value c'est le nom qu'on a choisit de donner à la variable qui contient la valeur renvoyé par le get
         //ici on a fait une fonction callback qui va nous permettre d'afficher la valeur, callback en fonction fléchée ES6
       )
+
+
+      //RxJS permet de créer nos propre observable, avec de nombreux opérateurs à notre disposition, auquel on attache les observer/callback comme dans le pattern normal
+                                                                                                    // ces traitements doivent être effectués dans la méthode pipe
+      timer(1000, 2000)
+      .pipe(
+        map(number => number + 1) //map est à importer, permet d'effectuer un traitement sur chaque élement de notre flux de données
+      )
+      .subscribe(
+        (number) => console.log(number)
+      )
+
   }
 
 }
